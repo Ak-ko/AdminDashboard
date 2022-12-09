db10ce109536a9dee4f25a0b9756a37b734dab22
Aung Khant Ko Ko <waterbubbles.ww6615@gmail.com>
2022-12-09 20:31:47 +0630

    addign files

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+const thisyear = document.querySelector('#year');
+const year = new Date().getFullYear();
+thisyear.textContent = year;
